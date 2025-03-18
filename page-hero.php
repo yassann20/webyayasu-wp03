e69f1c880079cdeb47c86558191fe7637923623c
yasuzaki25 <kaisei.yasuzaki@gmail.com>
2025-03-18 19:05:43 +0900

    2025/03/18テーマ修正

--- page-hero.php
+++ page-hero.php
@@ -1,16 +1,55 @@
 <?php get_header(); ?>
 
-<?php get_template_part('template-parts/section', 'hero') ?>
-
 <main>
     <div class="top-pading"></div><!--ヘッダーとの重なりを防ぐスペース-->
     <section class="about">
-        <div class="img">
-            <img src="" alt="" class="hero-profile_img" />
+        <div class="hero-profile">
+            <div class="img">
+                <img src="" alt="" class="hero-profile_img" />
+            </div>
+            <ul>
+                <li>氏名 : <ruby>安崎<rt>ヤスザキ</rt></ruby>&nbsp;<ruby>海星<rt>カイセイ</rt></ruby></li>
+                <li>職種 : エンジニア</li>
+                <li>出身地 : 北海道 札幌市</li>
+            </ul>
         </div>
-        <p>初めまして。この度はお忙しい中、貴重なお時間を割いて当サイトにお越しいただき、ありがとうございます。フロントエンドエンジニアの安崎海星（やすざき かいせい）と申します。<br>現在、私は地元札幌市のシステム開発会社でJavaを使用したバックエンド開発を行いながら、個人でフロントエンド開発の仕事をしています。基本的なウェブページのコーディングスキルに加え、PHPを使用したサーバーサイド技術や、Javaを用いたバックエンド開発技術も有しており、フロントエンドからバックエンドまで幅広い知識を持っています。<br>現在、クラウドソーシングなどを通じてフロントエンドのコーディング業務を中心にお受けしております。もしご縁があれば、お気軽にお仕事に関するご相談をいただければと思っております。</p>
+        <div class="hero-text">
+
+            <p>初めまして。この度はお忙しい中、貴重なお時間を割いて当サイトにお越しいただき、ありがとうございます。フロントエンドエンジニアの安崎と申します。<br>現在、私は地元札幌市のシステム開発会社でJavaを使用したバックエンド開発を行いながら、個人でフロントエンド開発の仕事をしています。基本的なウェブページのコーディングスキルに加え、PHPを使用したサーバーサイド技術や、Javaを用いたバックエンド開発技術も有しており、フロントエンドからバックエンドまで幅広い知識を持っています。<br>現在、クラウドソーシングなどを通じてフロントエンドのコーディング業務を中心にお受けしております。もしご縁があれば、お気軽にお仕事に関するご相談をいただければと思っております。</p>
+        </div>
+    </section>
+
+    <section class="background">
+        <table>
+            <tbody>
+                <tr>
+                    <td colspan="4" style="text-align: center;">これまでの業務経歴</td>
+                </tr>
+                <tr>
+                    <td>令和2年8月～現在</td>
+                    <td>株式会社クラウドワークス</td>
+                    <td>個人デザイナーとして活動している方との業務委託</td>
+                    <td>・住宅系LPのコーディング(複数本)<br>・フィットネスジムLPのコーディング(複数本)<br>・wordpressカスタマイズ<br>・お問い合わせフォームのコーディング(サーバーサイド含む)などを担当<br>使用スキル ( html / css / javascript / php / mysql / github )</td>
+                </tr>
+                <tr>
+                    <td>令和3年8月～現在</td>
+                    <td>オリクション株式会社</td>
+                    <td>フロントエンドエンジニアとして業務委託</td>
+                    <td>大手医薬品会社様およびサプリメント会社様のLPを月に数本規模でデザインの再現性を重視したコーディングを担当<br>使用スキル ( html / css / javascript / react.js / github )</td>
+                </tr>
+                <tr>
+                    <td>令和7年1月～現在</td>
+                    <td>株式会社ホットスタッフ</td>
+                    <td>バックエンドエンジニアとして入社</td>
+                    <td>企業様向けの顧客情報管理サイトのフロントエンド・バックエンドコーディングを担当しています。<br>使用スキル ( html / css / javascript / java / PostgreSQL / github )</td>
+                </tr>
+
+            </tbody>
+        </table>
     </section>
     <?php get_template_part('template-parts/section', 'skill') ?>
+
+
 
     <?php get_template_part('template-parts/section', 'contact') ?>
 

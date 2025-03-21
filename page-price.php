868fe89a75ba7c2345224cdea350f497a556a101
yasuzaki25 <kaisei.yasuzaki@gmail.com>
2025-03-22 05:04:41 +0900

    auto-commit

--- page-price.php
+++ page-price.php
@@ -2,23 +2,6 @@
 
 <main>
     <div class="top-pading"></div><!--ヘッダーとの重なりを防ぐスペース-->
-    <section class="about">
-        <div class="hero-profile">
-            <div class="img">
-                <img src="" alt="" class="hero-profile_img" />
-            </div>
-            <ul>
-                <li>氏名 : <ruby>安崎<rt>ヤスザキ</rt></ruby>&nbsp;<ruby>海星<rt>カイセイ</rt></ruby></li>
-                <li>職種 : エンジニア</li>
-                <li>出身地 : 北海道 札幌市</li>
-            </ul>
-        </div>
-        <div class="hero-text">
-
-            <p>初めまして。この度はお忙しい中、貴重なお時間を割いて当サイトにお越しいただき、ありがとうございます。フロントエンドエンジニアの安崎と申します。<br>現在、私は地元札幌市のシステム開発会社でJavaを使用したバックエンド開発を行いながら、個人でフロントエンド開発の仕事をしています。基本的なウェブページのコーディングスキルに加え、PHPを使用したサーバーサイド技術や、Javaを用いたバックエンド開発技術も有しており、フロントエンドからバックエンドまで幅広い知識を持っています。<br>現在、クラウドソーシングなどを通じてフロントエンドのコーディング業務を中心にお受けしております。もしご縁があれば、お気軽にお仕事に関するご相談をいただければと思っております。</p>
-        </div>
-    </section>
-
     <section class="background">
         <div class="sections-text-fixed">
             <div class="sections-title">

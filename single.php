0ce9ff1bc98b95885df4b7dbaed785ba88e38891
yasuzaki25 <kaisei.yasuzaki@gmail.com>
2025-03-24 11:52:47 +0900

    2025/03/24 テーマ修正完了

--- single.php
+++ single.php
@@ -47,51 +47,61 @@
                 </div>
                 <div class="pagination">
                     <ul>
-                <!--通常の投稿記事とカスタム投稿記事が混ざった記事リンクを取得する処理-->
-                <?php 
-                //現在の投稿IDと投稿タイプを取得
-                $current_id = get_the_ID();
-                $current_type = get_post_ID();
-                
-                ?>
-                        <!--カスタム投稿(section02)のリンクを取得する場合-->
-                        <?php if (is_singular('section02')): ?>
-                            <li>
-                                <?php
-                                $prev_post = get_previous_post(true, '', 'section02');
-                                if (($prev_post)):
-                                ?>
-                                    <a id="prev" href="<?php echo get_permalink($prev_post->ID); ?>">← prev</a>
-                                <?php endif; ?>
-                            </li>
-                            <li>
-                                <?php
-                                $next_post = get_next_post(true, '', 'section02');
-                                if (($next_post)):
-                                ?>
-                                    <a id="next" href="<?php echo get_permalink($next_post->ID); ?>">next →</a>
-                                <?php endif; ?>
-                            </li>
+                        <!--通常の投稿記事とカスタム投稿記事が混ざった記事リンクを取得する処理-->
+                        <?php
+                        //現在の投稿IDと投稿タイプを取得
+                        $current_id = get_the_ID();
+                        $current_type = get_post_type();
 
-                            <!--通常の投稿リンクを取得する場合-->
-                        <?php elseif (is_single()): ?>
+                        //投稿とカスタム投稿(section02)を指定
+                        $post_types = array('post', 'section02');
+
+                        // 現在の投稿より古い記事を取得（前の記事）
+                        $prev_post = get_posts(array(
+                            'post_type'      => $post_types,
+                            'posts_per_page' => 1,
+                            'orderby'        => 'date',
+                            'order'          => 'DESC',
+                            'post_status'    => 'publish',
+                            'exclude'        => $current_id, // 現在の投稿を除外
+                            'date_query'     => array(
+                                array(
+                                    'before' => get_the_date('Y-m-d H:i:s', $current_id) // 現在の投稿より前
+                                )
+                            )
+                        ));
+
+                        // 現在の投稿より新しい記事を取得（次の記事）
+                        $next_post = get_posts(array(
+                            'post_type'      => $post_types,
+                            'posts_per_page' => 1,
+                            'orderby'        => 'date',
+                            'order'          => 'ASC',
+                            'post_status'    => 'publish',
+                            'exclude'        => $current_id, // 現在の投稿を除外
+                            'date_query'     => array(
+                                array(
+                                    'after' => get_the_date('Y-m-d H:i:s', $current_id) // 現在の投稿より後
+                                )
+                            )
+                        ));
+                        ?>
+                        <!--カスタム投稿(section02)のリンクを取得する場合-->
+                        <!--前の記事を取得-->
                             <li>
                                 <?php
-                                $prev_post = get_previous_post();
-                                if (($prev_post)):
+                                if (!empty($prev_post)):
                                 ?>
-                                    <a id="prev" href="<?php echo get_permalink($prev_post->ID); ?>">← prev</a>
+                                    <a id="prev" href="<?php echo get_permalink($prev_post[0]->ID); ?>">← prev</a>
                                 <?php endif; ?>
                             </li>
                             <li>
                                 <?php
-                                $next_post = get_next_post();
-                                if (($next_post)):
+                                if (!empty($next_post)):
                                 ?>
-                                    <a id="next" href="<?php echo get_permalink($next_post->ID); ?>">next →</a>
+                                    <a id="next" href="<?php echo get_permalink($next_post[0]->ID); ?>">next →</a>
                                 <?php endif; ?>
                             </li>
-                        <?php endif; ?>
                     </ul>
                 </div>
             </section>
